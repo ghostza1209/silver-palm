6420c2fb69a53048851ee4970ab96c4111d3a520
yoss <yoss@fazwaz.com>
2024-03-10 14:02:31 +0700

    feat: add contact us functionality

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,5 +1,40 @@
 "use client";
-export default function ContactUs() {
+
+import React, { SyntheticEvent, useState } from "react";
+import toast from "react-hot-toast";
+
+export default function ContactUs(): React.JSX.Element {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log(e.target);
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    setIsSubmitting(true);
+
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxkuaLvpI4J_dD1K0nB8zkK7PQuZiFuu4JwtSrKrRHZZzH3gQV1xOaMJAWDWoO2R5zSqg/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((data) => {
+        toast.success(
+          "We've received your inquiry and will be in contact with you shortly.",
+          {
+            duration: 10000,
+          }
+        );
+        setIsSubmitting(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsSubmitting(false);
+      });
+  };
+
   return (
     <div className="bg-white px-6 py-20">
       <div className="mx-auto max-w-2xl text-center">
@@ -10,11 +45,7 @@ export default function ContactUs() {
           Please complete the form below and we will get back to you soon
         </p>
       </div>
-      <form
-        action="#"
-        method="POST"
-        className="mx-auto mt-16 max-w-xl sm:mt-20"
-      >
+      <form className="mx-auto mt-16 max-w-xl sm:mt-10" onSubmit={onSubmit}>
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label
@@ -59,6 +90,7 @@ export default function ContactUs() {
             </label>
             <div className="mt-2.5">
               <input
+                required
                 type="email"
                 name="email"
                 id="email"
@@ -80,7 +112,7 @@ export default function ContactUs() {
                 name="phone-number"
                 id="phone-number"
                 autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -104,10 +136,11 @@ export default function ContactUs() {
         </div>
         <div className="mt-10">
           <button
+            disabled={isSubmitting}
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Submit
+            {isSubmitting ? "Submitting..." : "Submit"}
           </button>
         </div>
       </form>
