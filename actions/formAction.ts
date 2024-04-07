0026bd6f1a2c3f1c4641f325c99ff300fbae1b84
Yoss <ghostza1209@gmail.com>
2024-04-07 13:01:48 +0700

    fix: revalidate homepage path

--- actions/formAction.ts
+++ actions/formAction.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { AddContactToGs } from "@/types/form";
-import {format} from 'date-fns';
+import { format } from "date-fns";
+import { revalidatePath } from "next/cache";
 
 export async function addContactToGs(data: AddContactToGs) {
   const formData = new FormData();
@@ -10,7 +11,7 @@ export async function addContactToGs(data: AddContactToGs) {
   formData.append("phone-number", data.phoneNumber);
   formData.append("email", data.email);
   formData.append("message", data.message);
-  formData.append('date', format(new Date(), 'MM/dd/yyyy H:i:s'));
+  formData.append("date", format(new Date(), "MM/dd/yyyy H:i:s"));
 
   fetch(
     "https://script.google.com/macros/s/AKfycby3wN58jv_U6UNpR_bFAfF5HjDsdpol3-zI2dCxuruZ87oNZCu31OAn3fLl03lMyl14uw/exec",
@@ -25,5 +26,8 @@ export async function addContactToGs(data: AddContactToGs) {
       errors: error.message,
     };
   });
+
+  revalidatePath("/");
+
   return { success: true, errors: [] };
 }
